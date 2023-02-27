947f8a65b98e4314b19c624b730a4d1e9b7b3cd3
LuisC0611 <cruzmendoza6@gmail.com>
2023-02-27 23:27:03 +0000

    New

--- src/app.js
+++ src/app.js
@@ -5,7 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.genCard = function() {
+  let suit = ["♥", "♠", "♣", "♦"];
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function randomCard() {
+    let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+    let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+    document.querySelector(".header").innerHTML = randomSuit;
+    document.querySelector(".body").innerHTML = randomNumber;
+    document.querySelector(".footer").innerHTML = randomSuit;
+
+    if (randomSuit == "♥" || randomSuit == "♦") {
+      document.querySelector("#cardcontainer").style.color = "red";
+    } else if (randomSuit == "♠" || randomSuit == "♣") {
+      document.querySelector("#cardcontainer").style.color = "black";
+    }
+  }
+
+  randomCard();
 };
+window.genCard();
